517eb2e8ead329240000b5449fe460cd0dbe3c09
vitru <vitru.luigi@gmail.com>
2021-02-26 14:43:32 +0100

    rename component Main to Body

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import Main from './components/Main/Main';
+import Body from './components/Body/Body';
 import './App.scss';
 import { data } from './utils/data.js';
 
@@ -9,7 +9,7 @@ const { logo, alt, cover, title, description, company, name } = data;
 const App = () => (
 	<div className='App'>
 		<Header src={logo} alt={alt} name={name} />
-		<Main src={cover} alt={alt} title={title} description={description} />
+		<Body src={cover} alt={alt} title={title} description={description} />
 		<Footer company={company} />
 	</div>
 );
--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,12 +1,12 @@
 import { Component } from 'react';
-import './Main.scss';
+import './Body.scss';
 import PropTypes from 'prop-types';
 
-class Main extends Component {
+class Body extends Component {
 	render() {
 		const { src, alt, title, description } = this.props;
 		return (
-			<main className='Main'>
+			<main className='Body'>
 				<img src={src} alt={alt} />
 				<h1>{title}</h1>
 				<h2>{description}</h2>
@@ -15,9 +15,9 @@ class Main extends Component {
 	}
 }
 
-export default Main;
+export default Body;
 
-Main.propTypes = {
+Body.propTypes = {
 	src: PropTypes.string.isRequired,
 	title: PropTypes.string.isRequired,
 	description: PropTypes.string.isRequired,
